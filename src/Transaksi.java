4328346007aa30953f70a25fe4869203b86ad5fd
dirarmdn <dhiraramadini@gmail.com>
2022-05-18 18:39:34 +0700

    add ifelse

--- src/Transaksi.java
+++ src/Transaksi.java
@@ -6,11 +6,19 @@ public class Transaksi implements Menu{
     }
 
     public void penarikan(int jumlahTarikan){
-        saldo = saldo - jumlahTarikan;
+        if (saldo < 20000){
+            System.out.println("Maaf saldo tidak mencapai minimal penarikan, segera lakukan penyetoran");
+        } else {
+            saldo = saldo - jumlahTarikan;
+        }
     }
 
     public void transfer(int s){
-        saldo = saldo - s;
+        if (saldo < 20000){
+            System.out.println("Maaf saldo tidak mencapai minimal transfer, segera lakukan penyetoran");
+        } else {
+            saldo = saldo - s;
+        }
     }
 
     public void cetakSaldo()
